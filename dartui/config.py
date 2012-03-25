6ec3d05a2409a2b6b871d94b31cd3be6d2c71313
Chris Lucas <chris@chrisjlucas.com>
2012-03-24 21:05:44 -0700

    added code to handle v1.0.0 users switching to the new db system

--- dartui/config.py
+++ dartui/config.py
@@ -14,11 +14,29 @@ class ConfigDir:
         
         self.refresh()
         
+        # quick and dirty way to see if config file is using the old db system from v1.0.0
+        # (this can be removed sometime in the distant future)
+        db = self.get_db()
+        tables = db._get_tables()
+        if "settings" in tables and "table_versions" not in tables:
+            print("old db style found, update settings table")
+            update_settings_table = True
+        else:
+            update_settings_table = False
+        
         self.table_versions = self.get_table_versions()
+        
+        # check if tables are up to date, and update them if not
+        for table in sql.tables:
+            if table != "table_versions" and table in self.table_versions: # already handled by get_table_versions
+                db = self.get_db(sql.tables[table]) # this will create the table if it doesn't already exist
+                if sql.tables[table].version < self.table_versions[table]: db.update_table_struct()
+                elif table == "settings" and update_settings_table: db.update_table_struct()
 
     def get_table_versions(self):
         table_versions = {}
         db = self.get_db(sql.tables["table_versions"])
+        db.insert_defaults() # insert new tables (if any)
         for row in db.get_table_contents():
             table_versions[row["name"]] = row["version"]
         return(table_versions)
